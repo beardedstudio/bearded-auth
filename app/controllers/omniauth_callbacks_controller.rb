d6082ae0d3dafa8fa51b2e49721b0aa2bb1340ad
Brett Bender <brett@bearded.com>
2011-06-01 17:03:51 -0400

    Starting on omniauth callback controller and user methods for oauth support

---
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -0,0 +1,22 @@
+class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+
+  def facebook
+    @user = User.find_for_facebook_oauth(env["omniauth.auth"], current_user)
+    handle_omniauth_login "facebook", 'devise.facebook_data'
+  end
+   
+  def open_id
+    @user = User.find_for_openid_oauth(env['omniauth.auth'], current_user)
+    handle_omniauth_login "Google", 'devise.openid_data'
+  end
+  
+  def handle_omniauth_login omniauth_provider, session_key
+    if @user.persisted?
+      flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', :kind => omniauth_provider
+      sign_in_and_redirect @user, :event => :authentication
+    else
+      session[session_key] = env['omniauth.auth']
+      redirect_to edit_user_path(@user)
+    end    
+  end  
+end
--- app/models/bearded/user.rb
+++ app/models/bearded/user.rb
@@ -5,6 +5,65 @@ module Bearded
            :recoverable, 
            :rememberable, 
            :trackable, 
+           :registerable
            :validatable
   end
+  
+  #########################
+  # OAuth methods
+  #########################
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["user_hash"]
+        user.email = data["email"]
+      end
+    end
+  end
+  
+  def self.find_for_facebook_oauth(access_token, signed_in_resource=nil)
+    data = access_token['user_info']
+    if user = User.find_by_facebook_id(access_token['uid'])
+      user.update_facebook_info_from_access_token data
+      user
+    else # Create an user with a stub password. 
+      User.create!( :facebook_id => access_token['uid'],
+                    :email => data["email"], 
+                    :name => data["name"], 
+                    :password => Devise.friendly_token[0,20], 
+                    :facebook_url => data['urls']["Facebook"],
+                    :web_url => data['urls']['Website'],
+                    :profile_image_url => data['image']) 
+    end
+  end
+  
+  def self.find_for_openid_oauth(access_token, signed_in_resource=nil)
+    data = access_token['user_info']
+    if user = User.find_by_email(data["email"])
+      user
+    # Create a user with a stub password. 
+    else 
+      User.create!(:email => data["email"], :name => data["name"], :password => Devise.friendly_token[0,20]) 
+    end
+  end
+  
+  def update_facebook_info_from_access_token data
+    need_save = false
+
+    if self.name != data['name']
+      need_save = true
+      self.name = data['name'] 
+    end
+    
+    if self.profile_image_url != data['image']
+      need_save = true
+      self.profile_image_url = data['image']
+    end
+    
+    if self.email != data['email']
+      need_save = true
+      self.email = data['email']
+    end
+    
+    self.save if need_save
+  end  
 end
