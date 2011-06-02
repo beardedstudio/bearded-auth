72fd033949cd9888f5906c0c310b9934ba70547d
Brett Bender <brett@bearded.com>
2011-06-02 16:08:29 -0400

    Namespacing correctly in routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do  
-  devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks"}
+  namespace :bearded do
+    devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks"}, :module => "devise", :class_name => "Bearded::User"
+  end
 end
