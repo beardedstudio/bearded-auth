a87aa78ec83858279350058715dcea551e9c3e6b
Dominic Dagradi <dominic@bearded.com>
2011-06-09 14:18:56 -0400

    devise routes on /sessions, sign_in/sign_out shortcuts

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do  
-  devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks"}, :module => "devise", :class_name => "User"
+  devise_for :users, :path => "sessions", :controllers => { :omniauth_callbacks => "omniauth_callbacks"}, :module => "devise", :class => "User" do
+
+    get "sign_in", :to => "devise/sessions#new", :as => "sign_in"
+    get "sign_out", :to => "devise/sessions#destroy", :as => "sign_out"
+
+  end
+  
+    
 end
--- lib/bearded-auth/version.rb
+++ lib/bearded-auth/version.rb
@@ -1,5 +1,5 @@
 module Bearded
   module Auth
-    VERSION = "0.5"
+    VERSION = "0.5.1"
   end
 end
