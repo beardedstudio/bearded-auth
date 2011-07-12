7e467fcb3e18e69b52425ff85af8bc4ca922a455
Brett Bender <brett@bearded.com>
2011-07-12 15:15:23 -0400

    Adding support for an initializer and custom overridden controllers for devise.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do  
-  devise_for :users, :path => "sessions", :controllers => { :omniauth_callbacks => "omniauth_callbacks"}, :module => "devise", :class => "User" do
-
+  devise_for :users, :path => "sessions", :controllers => Bearded::Auth.devise_controllers, :module => "devise", :class => "User" do
     get "sign_in", :to => "devise/sessions#new", :as => "sign_in"
     get "sign_out", :to => "devise/sessions#destroy", :as => "sign_out"
-
   end
-  
-    
 end
--- lib/bearded-auth.rb
+++ lib/bearded-auth.rb
@@ -7,7 +7,20 @@ require 'oa-openid'
 
 module Bearded
   module Auth
+
+    mattr_reader :devise_controllers
+    @@devise_controllers = { :omniauth_callbacks => "omniauth_callbacks"}
+
     class Engine < Rails::Engine
+    end
+    
+    def self.setup
+      yield self
+    end
+    
+    def self.override_devise_controllers(options)
+      raise Exception.new("The argument to override_devise_controllers must be a hash in the same format as accepted by devise_for") unless options.is_a?(Hash) 
+      @@devise_controllers = @@devise_controllers.merge(options)
     end
   end
 end
